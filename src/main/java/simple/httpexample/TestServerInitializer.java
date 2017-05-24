77f3bbecc1f2736098564fc416d93e416e2051f6
hyperglory <hyperglory@foxmail.com>
2017-05-24 23:54:48 +0800

    first commit

---
+++ src/main/java/simple/httpexample/TestServerInitializer.java
@@ -0,0 +1,20 @@
+package httpexample;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpServerCodec;
+
+/**
+ * @author hyperglory
+ * @date 2017/5/17 20:26
+ */
+public class TestServerInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+
+        pipeline.addLast("httpServerCodec", new HttpServerCodec());
+        pipeline.addLast("testHttpServerHandler", new TestHttpServerHandler());
+    }
+}
---
+++ src/main/java/simple/socketexample/TestSocketServerHandler.java
@@ -0,0 +1,24 @@
+package socketexample;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+import java.util.UUID;
+
+/**
+ * @author hyperglory
+ * @date 2017/5/24 22:50
+ */
+public class TestSocketServerHandler extends SimpleChannelInboundHandler<String> {
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+        System.out.println(ctx.channel().remoteAddress() + ", " + msg);
+        ctx.writeAndFlush("from server: " + UUID.randomUUID());
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
