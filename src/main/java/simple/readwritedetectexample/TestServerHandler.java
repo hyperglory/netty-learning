aad5964814e5831974d0bcddffc94100edf94d21
hyperglory <hyperglory@foxmail.com>
2017-05-26 15:00:00 +0800

    read write detect example

---
+++ src/main/java/simple/readwritedetectexample/TestServerHandler.java
@@ -0,0 +1,36 @@
+package simple.readwritedetectexample;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.timeout.IdleStateEvent;
+
+/**
+ * @author hyperglory
+ * @date 2017/5/26 14:28
+ */
+public class TestServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+        if (evt instanceof IdleStateEvent) {
+            IdleStateEvent event = (IdleStateEvent) evt;
+            String eventType = null;
+
+            switch (event.state()) {
+                case READER_IDLE:
+                    eventType = "读空闲";
+                    break;
+                case WRITER_IDLE:
+                    eventType = "写空闲";
+                    break;
+                case ALL_IDLE:
+                    eventType = "读写空闲";
+                    break;
+            }
+
+            System.out.println(ctx.channel().remoteAddress() + " 超时事件：" + eventType);
+
+            ctx.channel().close();
+        }
+    }
+}
---
+++ src/main/java/simple/readwritedetectexample/TestServerInitializer.java
@@ -0,0 +1,23 @@
+package simple.readwritedetectexample;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.timeout.IdleStateHandler;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author hyperglory
+ * @date 2017/5/26 14:21
+ */
+public class TestServerInitializer extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+
+        pipeline.addLast(new IdleStateHandler(5, 7, 10, TimeUnit.SECONDS));
+        pipeline.addLast(new TestServerHandler());
+    }
+}
